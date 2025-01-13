d72fbff44ad4dfbe27c1da50a0eedb927db26795
Eugene Yakush <fonBlaser@gmail.com>
2025-01-14 01:01:16 +0200

    Remove warning

--- Source/Tests/SvcCom.Tests.Unit.Referenced/ReferencedTestConfig.cs
+++ Source/Tests/SvcCom.Tests.Unit.Referenced/ReferencedTestConfig.cs
@@ -5,7 +5,7 @@ namespace SvcCom.Tests.Unit.Referenced;
 public class ReferencedTestConfig
 {
     internal static string TargetAssemblyName
-        => typeof(IWiki).Assembly.GetName().Name;
+        => typeof(IWiki).Assembly.GetName().Name ?? string.Empty;
 
     internal static string TargetAssemblyPath
         => typeof(IWiki).Assembly.Location;
